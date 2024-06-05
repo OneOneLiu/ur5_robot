bef71ba8fd8af16d71344bf49276ef1a80647bcb
Server <daohui.liu@mail.utoronto.ca>
2024-06-05 20:23:56 +0000

    add movement check

--- src/nodes/move_service.py
+++ src/nodes/move_service.py
@@ -6,6 +6,8 @@ from ur5_robot.srv import MoveToPose, MoveToPoseResponse
 def move_to_pose_service(req):
     global ur_robot  # Use a global reference for simplicity
     # Alternatively, for more detailed output, especially useful in debugging
+    ur_robot.get_current_pose()
+
     rospy.loginfo("Received pose goal:\nPosition - x: {0}, y: {1}, z: {2}\nOrientation - x: {3}, y: {4}, z: {5}, w: {6}".format(
         req.pose_goal.position.x, 
         req.pose_goal.position.y, 
@@ -16,7 +18,12 @@ def move_to_pose_service(req):
         req.pose_goal.orientation.w))
 
     try:
-        ur_robot.go_to_cartesian_pose(req.pose_goal)
+        fraction = ur_robot.go_to_cartesian_pose(req.pose_goal)
+        if fraction < 1.0:
+            rospy.logwarn("Only %s%% of the path was planned. Please check if the target pose is reachable." % (fraction * 100))
+            # rospy.log("Path planning failed with fraction: %f", fraction)
+            return MoveToPoseResponse(False)
+
         rospy.loginfo("Robot moved to pose successfully.")
         return MoveToPoseResponse(True)
 
